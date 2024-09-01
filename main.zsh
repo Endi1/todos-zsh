2f9400d82f81dc7d43c7e8ac9b8a1d5a5d3ea473
Endi Sukaj <endisukaj@gmail.com>
2024-09-02 00:35:03 +0200

    Initial commit

---
+++ main.zsh
@@ -0,0 +1,34 @@
+todos() {
+    if [[ "$1" == "list" ]]; then
+        todos-list $2
+    fi
+
+    if [[ "$1" == "capture" ]]; then
+        todos-capture
+    fi
+}
+
+todos-list() {
+    if [[ "$1" == "" ]]; then
+        result=$(grep -n "\- \[ \]" ~/Notes/todo.md | fzf | awk '{print $1}' | grep -o '[[:digit:]]\+')
+        if [[ "$result" == "" ]]; then
+        else
+            vim +$result ~/Notes/todo.md
+        fi
+    elif [[ "$1" == "A" || "$1" == "B" || "$1" == "C" ]]; then
+        result=$(grep -n "\- \[ \]" ~/Notes/todo.md | grep "($1)" | fzf | awk '{print $1}' | grep -o '[[:digit:]]\+')
+        if [[ "$result" == "" ]]; then
+        else
+            vim +$result ~/Notes/todo.md
+        fi
+    else
+        echo "Please pick a correct priority level"
+    fi
+}
+
+todos-capture() {
+    filename=$RANDOM
+    vim /tmp/$filename.md
+    cat /tmp/$filename.md >> ~/Notes/todo.md
+}
+
